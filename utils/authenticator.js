af04bffaf5bf77b035d78540eef6628a8b361b69
Kaylleur <dev@thomas-brunet.fr>
2017-02-12 21:26:29 +0100

    update authenticator
    authenticator now check only headers and let pass OPTIONS request for CORS process

--- utils/authenticator.js
+++ utils/authenticator.js
@@ -10,28 +10,32 @@ const jwt = require('jsonwebtoken');
 // route middleware to verify a token
 router.use(function(req, res, next) {
 
-	// check header or url parameters or post parameters for token
-	let token = req.body.token || req.query.token || req.headers['x-access-token'];
+	if(req.method == 'OPTIONS') next();
+	else {
+		// check header or url parameters or post parameters for token
+		let token = req.headers['x-access-token'];
 
-	// decode token
-	if (token) {
-		// verifies secret and checks exp
-		jwt.verify(token, config.jwt.secret, {algorithms: config.session.algorithm}, function(err, verify) {
-			if (err) {
-				return res.status(400).send({message: 'FAILED TO AUTHENTICATE TOKEN', err: err});
-			} else {
-				// if everything is good, save to request for use in other routes
-				req.user = verify._doc;
-				next();
-			}
-		});
+		// decode token
+		if (token) {
+			// verifies secret and checks exp
+			jwt.verify(token, config.jwt.secret, {algorithms: config.session.algorithm}, function(err, verify) {
+				if (err) {
+					return res.status(400).send({message: 'FAILED TO AUTHENTICATE TOKEN', err: err});
+				} else {
+					// if everything is good, save to request for use in other routes
+					req.user = verify._doc;
+					next();
+				}
+			});
 
-	} else {
-		// if there is no token
-		// return an error
-		return res.status(403).send({
-			message: 'NO TOKEN PROVIDED'
-		});
+		} else {
+			// if there is no token
+			// return an error
+			return res.status(403).send({
+				message: 'NO TOKEN PROVIDED'
+			});
+		}
+
 	}
 });
 
